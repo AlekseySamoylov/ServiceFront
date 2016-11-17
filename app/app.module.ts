c5adff744379695173a05de4c9a8bef6cdbbebe9
Алексей Ширяев <alexpl_07@inbox.ru>
2016-11-17 15:33:32 +0500

    1) Добавлен app.config.ts 2) Ссылка на webApi в виде настройки apiEndpoint

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 import { AppComponent }  from './app.component';
 import { routing } from './app.routing';
 
+import { APP_CONFIG, AppConfig } from './app.config';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CompaniesComponent } from './components/pages/companies.component';
 import { WorksComponent } from './components/pages/works.component';
@@ -12,6 +14,8 @@ import { WorksComponent } from './components/pages/works.component';
 @NgModule({
   imports:      [ BrowserModule, HttpModule, routing ],
   declarations: [ AppComponent, NavbarComponent, CompaniesComponent, WorksComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [AppComponent],
+  providers: [ { provide: APP_CONFIG, useValue: AppConfig }
+    ]
 })
 export class AppModule { }
--- app/services/works.service.ts
+++ app/services/works.service.ts
@@ -1,16 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { APP_CONFIG, IAppConfig } from '../app.config';
 
 @Injectable()
 export class WorksService {
 
-    constructor(private _http: Http) {
+    serviceUrl: String;    
 
+    constructor(@Inject(APP_CONFIG) private config: IAppConfig, private _http: Http) {
+        this.serviceUrl = config.apiEndpoint;
     }
 
     getWorkGroups() {
-        return this._http.get('http://www.alekseysamoylov.com:8080/serviceiii/prices')
+        return this._http.get( this.serviceUrl + '/prices')
             .map(res => res.json());
     }
 }
