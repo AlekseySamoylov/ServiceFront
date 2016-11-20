c1067244259a1c381b6b50a48d3fa50044419a1d
alexpl <alexpl_07@inbox.ru>
2016-11-20 23:46:04 +0500

    makeup (+ bootstrap.js, jquery)

--- app/components/pages/works.component.ts
+++ app/components/pages/works.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WorksService } from '../../services/works.service';
 
 @Component({
@@ -6,13 +6,29 @@ import { WorksService } from '../../services/works.service';
     selector: 'works',
     templateUrl: 'works.component.html'
 })
-export class WorksComponent {
+export class WorksComponent implements OnInit {
     workGroups: any[];
+    collapse: string;
+    collapseInfo: string;
+    isExpand: Boolean;
 
     constructor(private _worksService: WorksService) {
+    }
+
+    ngOnInit(): void {
         this._worksService.getWorkGroups().subscribe(workGroups => {
             this.workGroups = workGroups;
             console.log(this.workGroups.length);
         });
+
+        this.collapse = "";
+        this.collapseInfo = "Развернуть все";
+        this.isExpand = false;
+    }
+
+    hideShowAll(): void {
+        this.isExpand = !this.isExpand;
+        this.collapse = this.isExpand ? "in" : "";
+        this.collapseInfo = this.isExpand ? "Свернуть все" : "Развернуть все";
     }
 }
