32dc72f2c538ca777dc26bc137106da2b6f29c92
alexpl <alexpl_07@inbox.ru>
2016-11-21 20:59:41 +0500

    added button (Add)

--- app/components/pages/companies.component.ts
+++ app/components/pages/companies.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CompaniesService } from '../../services/companies.service';
 
 @Component({
@@ -6,10 +6,13 @@ import { CompaniesService } from '../../services/companies.service';
     selector: 'companies',
     templateUrl: 'companies.component.html'
 })
-export class CompaniesComponent {
+export class CompaniesComponent implements OnInit {
     companies: any[];
 
     constructor(private _companiesService: CompaniesService) {
+    }
+
+    ngOnInit(): void {
         this._companiesService.getCompanies().subscribe(companies => {
             this.companies = companies;
         });
